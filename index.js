742ffa170647b798dbe9f6f8325ca972660ceb11
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-07 13:29:31 -0400

    commit tree ece8d02b2cd1e51bab9f9ae84d25f48a8125a55c

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const split = tutorials.map(function (element) {
+    return element.split(" ");
+  })
+  return split.map(function(element) {
+    return element.map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  })
 }
